bc2aa0c59ebf225f5f426f98db8bf599a246f19c
Saurav1999 <singhsaurav1999@gmail.com>
2021-02-21 04:08:02 +0530

    UI optimization

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,42 @@
 import React from "react";
-import Header from "./Header";
+// import Header from "./Header";
+import resume from "../assets/resume.png";
+// import header_background_sm from "../assets/header_background_sm.svg";
 
 function Hero() {
   return (
     <div className="section hero grid">
-      <Header />
+      {/* <Header /> */}
+      {/* <div className="header-background">
+        <img
+          src={header_background_sm}
+          alt="background"
+        ></img>
+      </div> */}
+      <div className="section header grid">
+        <div className="home__logo">
+          <h1>S</h1>
+        </div>
+        {/* <div className="header-background">
+        <img
+          src={header_background_sm}
+          alt="background"
+        ></img>
+      </div> */}
+        <nav>
+          <div className="home_nav">
+            <a href="#skills">Skills</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact Me</a>
+
+            <button>
+              <a href="https://drive.google.com/file/d/1yZASuaytfxZlOVV4OPaSWqXemqhAY0zO/view?usp=sharing">
+                <img className="icon" src={resume} alt="cv icon"></img>
+              </a>
+            </button>
+          </div>
+        </nav>
+      </div>
       <div className="hero-content grid">
         <h2 className="hero__greeting">Hi! I'm</h2>
         <h2 className="hero__name">Saurav Singh</h2>
@@ -33,12 +65,13 @@ function Hero() {
             </a>
           </div>
         </div>
-       
       </div>
-      <div className="scroll-down">
+      {/* <div className="scroll-down">
           <div className="mouse"></div>
           <p className="scroll-text">Scroll</p>
-        </div>
+        </div> */}
+      <div class="scroll-indicator"><div class="scroll-indicator__icon"><i class="fa fa-chevron-down"></i><i class="fa fa-chevron-down"></i></div>Scroll down</div>
+
       {/* <div className="scroll-down-container">
         <a href="#about" className="scroll-down">
           <i class="fa fa-chevron-down"></i>
