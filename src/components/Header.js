ced398c9e9b7b575c9483d27d0d88cd67c8eb0e8
Saurav Singh <35275359+Saurav1999@users.noreply.github.com>
2021-10-10 01:23:09 +0530

    Changed Resume Link

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ function Header() {
           <a href="#contact">Contact Me</a>
           
           <button>
-            <a href="https://drive.google.com/file/d/1_a4tu77TeeWz3vrmbVV9IYmxNqtKyGIa/view?usp=sharing">
+            <a href="https://drive.google.com/file/d/1_a4tu77TeeWz3vrmbVV9IYmxNqtKyGIa/view?usp=sharing" >
             <img
               className="icon"
               src={resume}
